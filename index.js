2ff482e8587176165042f360c3ebf464ac106d19
EasyCreations <81872388+EasyCreations@users.noreply.github.com>
2022-12-29 22:12:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const { prefix, token } = require('./config.json');
 const fivereborn = require('fivereborn-query');
 const configs = require("./configs.json");
 const guild = client.guilds.cache.get("785961609318891540");
+
+const bot = client
+
+const fs = require('fs');
+const { format } = require('path');
+
+client.commands = new Discord.Collection();
